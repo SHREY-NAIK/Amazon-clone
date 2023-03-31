30fb68ffd76d02410cc9226e5df0631dc6afedab
SHREY <shrey.naik@gmail.com>
2023-04-01 00:00:46 +0530

    upgraded auth to vercel

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -8,6 +8,7 @@ export const authOptions = {
    GoogleProvider({
       clientId: process.env.GOOGLE_ID,
       clientSecret: process.env.GOOGLE_SECRET,
+      secret: process.env.NEXT_PUBLIC_SECRET,
     }),
     // ...add more providers here
   ],
